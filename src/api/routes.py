7ac34e8f382b123796b906d1bf3d788d2914247c
Blessing <blessingogbogu@gmail.com>
2025-04-29 02:28:32 +0000

    Initial commit for JWT project

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,8 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+import bcrypt
+from flask_jwt_extended import create_access_token
 
 api = Blueprint('api', __name__)
 
@@ -12,11 +14,20 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/login', methods=['POST'])
+def create_token():
+    data = request.json
+    email = data.get("email", None)
+    password = data.get("password", None)
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+     # Query your database for username and password
+    user = User.query.filter_by(email = email, password = password).first()
+    if user is None:
+        # The user was not found on the database
+        return jsonify({"msg": "Bad username or password"}), 401
+    
+    # Create a new token with the user id inside
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id })
 
-    return jsonify(response_body), 200
+    
