89de244e7d1e1047cbcad5390df41efd8fa0e2d8
Blessing <blessingogbogu@gmail.com>
2025-05-08 03:13:44 +0000

    Global state update

--- src/front/store.js
+++ src/front/store.js
@@ -1,37 +1,24 @@
 export const initialStore=()=>{
   return{
     message: null,
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ]
+    signup: [],
+    login: [],
+    token: []
   }
 }
 
 export default function storeReducer(store, action = {}) {
   switch(action.type){
-    case 'set_hello':
+    case 'signup':
       return {
         ...store,
-        message: action.payload
+        signup: [...store.signup, action.payload]
       };
-      
-    case 'add_task':
-
-      const { id,  color } = action.payload
-
-      return {
-        ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
-      };
+      case "login":
+        return {
+          ...store,
+          login: [...store.login, action.payload]
+        };
     default:
       throw Error('Unknown action.');
   }    
